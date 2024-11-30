8489a6a10417b8b2915a2fa958f4578fd4bd5a53
Nipuna <IT21166556@my.sliit.lk>
2024-11-30 20:06:48 +0530

    merged 💥

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,24 +1,2 @@
 import Product  from "../models/product.js";
 
-export function getProduct(req,res){
-
-  Product.find().then(
-
-    (productList)=>{
-      res.status(200).json({
-        list : productList
-      }) 
-    }
-  ).catch(
-    (err)=>{
-      res.json({
-        message : "Error"
-      })
-    }
-  )
-}
-
-export function createProduct(req,res){
-  
-}
-
