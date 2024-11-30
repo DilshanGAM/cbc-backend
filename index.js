1173ec68ef49ecb62270a1d067562cb2ea6efad4
Nipuna <IT21166556@my.sliit.lk>
2024-11-30 21:35:27 +0530

    order completed 🛒

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import mongoose from 'mongoose';
 import userRouter from './routes/userRouter.js';
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import productRouter from './routes/productRouter.js';
+import orderRouter from './routes/orderRouter.js';
 
 dotenv.config()
 
@@ -49,6 +51,8 @@ app.use(
 
 )
 app.use("/api/users",userRouter)
+app.use("/api/products",productRouter)
+app.use("/api/orders",orderRouter)
 
 
 app.listen(
