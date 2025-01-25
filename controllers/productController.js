aa1213643cbf3c13032c2461ce5648a9c6abcb42
Malith Dilshan <IT21166556@my.sliit.lk>
2025-01-25 21:26:13 +0530

    day 21✅

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,99 +1,113 @@
-import Product  from "../models/product.js";
+import Product from "../models/product.js";
 import { isAdmin } from "./userController.js";
 
-export function createProduct(req,res){
-
-  if(!isAdmin(req)){
+export function createProduct(req, res) {
+  if (!isAdmin(req)) {
     res.json({
-      message: "Please login as administrator to add products"
-    })
-    return
+      message: "Please login as administrator to add products",
+    });
+    return;
   }
 
-  const newProductData = req.body
+  const newProductData = req.body;
 
-  const product = new Product(newProductData)
+  const product = new Product(newProductData);
 
-  product.save().then(()=>{
-    res.json({
-      message: "Product created"
+  product
+    .save()
+    .then(() => {
+      res.json({
+        message: "Product created",
+      });
     })
-  }).catch((error)=>{
-    res.status(403).json({
-      message: error
-    })
-  })
+    .catch((error) => {
+      res.status(403).json({
+        message: error,
+      });
+    });
 }
 
-export function getProducts(req,res){
-  Product.find({}).then((products)=>{
-    res.json(products)
-  })
+export function getProducts(req, res) {
+  Product.find({}).then((products) => {
+    res.json(products);
+  });
 }
 
-export function deleteProduct(req,res){
-  if(!isAdmin(req)){
+export function deleteProduct(req, res) {
+  if (!isAdmin(req)) {
     res.status(403).json({
-      message: "Please login as administrator to delete products"
-    })
-    return
+      message: "Please login as administrator to delete products",
+    });
+    return;
   }
 
-  const productId = req.params.productId
+  const productId = req.params.productId;
 
-  Product.deleteOne(
-    {productId : productId}
-  ).then(()=>{
-    res.json({
-      message: "Product deleted"
+  Product.deleteOne({ productId: productId })
+    .then(() => {
+      res.json({
+        message: "Product deleted",
+      });
     })
-  }).catch((error)=>{
-    res.status(403).json({
-      message: error
-    })
-  })
+    .catch((error) => {
+      res.status(403).json({
+        message: error,
+      });
+    });
 }
 
-export function updateProduct(req,res){
-  if(!isAdmin(req)){
+export function updateProduct(req, res) {
+  if (!isAdmin(req)) {
     res.status(403).json({
-      message: "Please login as administrator to update products"
-    })
-    return
+      message: "Please login as administrator to update products",
+    });
+    return;
   }
 
-  const productId = req.params.productId
-  const newProductData = req.body
+  const productId = req.params.productId;
+  const newProductData = req.body;
 
-  Product.updateOne(
-    {productId : productId},
-    newProductData
-  ).then(()=>{
-    res.json({
-      message: "Product updated"
+  Product.updateOne({ productId: productId }, newProductData)
+    .then(() => {
+      res.json({
+        message: "Product updated",
+      });
     })
-  }).catch((error)=>{
-    res.status(403).json({
-      message: error
-    })
-  })
+    .catch((error) => {
+      res.status(403).json({
+        message: error,
+      });
+    });
 }
 
-export async function getProductById(req,res){
+export async function getProductById(req, res) {
+  try {
+    const productId = req.params.productId;
 
-  try{
-    const productId = req.params.productId
+    const product = await Product.findOne({ productId: productId });
 
-    const product = await Product.findOne({productId : productId})
-
-    res.json(product)
-    
-  }catch(e){
+    res.json(product);
+  } catch (e) {
     res.status(500).json({
-      e
-    })
+      e,
+    });
   }
-  
 }
 
+export async function searchProducts(req, res) {
+  const query = req.params.query;
+  try {
+    const products = await Product.find({
+      $or: [
+        { productName: { $regex: query, $options: "i" } },
+        { altNames: { $elemMatch: { $regex: query, $options: "i" } } },
+      ],
+    });
 
+    res.json(products);
+  } catch (e) {
+    res.status(500).json({
+      e,
+    });
+  }
+}
